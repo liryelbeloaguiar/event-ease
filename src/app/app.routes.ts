40c999c7b8a57adc972d9d22af1952407145996a
liryel <liryelaguiar57@gmail.com>
2025-07-22 21:55:15 -0300

    Meu primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    
+
+];
